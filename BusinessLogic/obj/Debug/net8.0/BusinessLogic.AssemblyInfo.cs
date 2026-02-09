9209d395a9280797b1c4e526b3f931ec49b96ae2
quyn03z <quyenvn03@gmail.com>
2026-02-09 09:14:55 +0700

    commit new

--- BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbf74251b0e1572f61060d61f951912282ae2b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
