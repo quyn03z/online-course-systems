ced9ac68fb734eef7e855a16c7caca23a78aff9a
quyn03z <quyenvn03@gmail.com>
2026-03-11 13:56:51 +0700

    add feat documents new

---
+++ DataAccess/Migrations/20260311025931_NewDocumentsModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewDocumentsModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Creator",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    DocumentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LessonId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsLocked = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.DocumentId);
+                    table.ForeignKey(
+                        name: "FK_Documents_Lesson_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lesson",
+                        principalColumn: "LessonId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_LessonId",
+                table: "Documents",
+                column: "LessonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "Course");
+        }
+    }
+}
---
+++ DataAccess/Repositories/Repo/DocumentsRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Models.LessonModel;
+using DataAccess.Repositories.Impl;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Repo
+{
+	public class DocumentsRepository : BaseRepository<Documents>, IDocumentsRepository
+	{
+		public DocumentsRepository(OCMSMSFContext context) : base(context)
+		{
+		}
+
+		public async Task<List<Documents>> GetAllsDocuments(int lessonId)
+		{
+			return await _dbSet.Where(x => x.LessonId == lessonId && x.IsLocked == false).ToListAsync();
+		}
+
+		public async Task<List<Documents>> GetAllsManaDocumentsAsync(int lessonId)
+		{
+			return await _dbSet.Where(x => x.LessonId == lessonId).ToListAsync();
+		}
+	}
+}
