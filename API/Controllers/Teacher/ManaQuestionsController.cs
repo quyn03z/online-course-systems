a6c1fd7972edae6dbd656fb86204cb9ac64e3a68
quyn03z <quyenvn03@gmail.com>
2026-03-16 10:06:31 +0700

    commit new branch

---
+++ API/Controllers/Teacher/ManaQuestionsController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogic.Services.Impl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Teacher
+{
+	public class ManaQuestionsController : BaseController
+	{
+		private readonly IQuestionsService _questionsService;
+
+		public ManaQuestionsController(IQuestionsService questionsService)
+		{
+			_questionsService = questionsService;
+		}
+	}
+}
