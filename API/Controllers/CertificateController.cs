010ad51d4514c4142beec7b08d336122c8a52c4f
quyn03z <quyenvn03@gmail.com>
2026-03-27 11:20:08 +0700

    add certificate

---
+++ API/Controllers/CertificateController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic.Models;
+using BusinessLogic.Services.Impl;
+using DataAccess.Models.CourseModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static BusinessLogic.Models.User;
+
+namespace API.Controllers
+{
+	public class CertificateController : BaseController
+	{
+		private readonly ICourseService _courseService;
+
+		public CertificateController(ICourseService courseService)
+		{
+			_courseService = courseService;
+		}
+
+		[HttpGet("download/{courseId}")]
+		public async Task<IActionResult> DownloadCertificateAsync(int courseId)
+		{
+			return Ok(ApiResult<string>.Success(await _courseService.DownloadCertificateAsync(courseId)));
+		}
+
+
+
+	}
+}
